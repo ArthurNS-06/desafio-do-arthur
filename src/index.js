22c54f5bb86b7227634778b99ab984df605cd813
ArthurNS-06 <arthurnascimentos2098@gmail.com>
2026-07-29 23:37:41 -0300

    Fiz o código com usando JavaScript, utilizando loop para adicionar mais de um herói para commitar mais de um exemplo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-let nome = "felipe";
-console.log("Oi");
+let herois = [
+    {nome: "Kratos Goat", xp: 12000 },
+    {nome: "Joel Miller", xp: 4000 },
+    {nome: "Arthur Morgan", xp: 8000 },
+    {nome: "Nathan Drake", xp: 7000 },
+    {nome: "Peter Parker", xp: 9000 },
+];
+
+for (let i = 0; i < herois.length; i++) {
+
+    let nomeHeroi = herois[i].nome;
+    let xp = herois[i].xp;
+    let nivel;
+
+    if (xp <= 1000) {
+        nivel = "Ferro";
+    } else if (xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp <= 8000) {
+        nivel = "Platina"; 
+    } else if (xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp <=10000) {
+        nivel = "Imortal";   
+    } else {
+        nivel = "Radiante";
+    }
+
+    console.log("O Herói de nome " + nomeHeroi + " possui " + xp + " de experiência e está classificado como " + nivel + ".");
+}
